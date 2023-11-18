cd5995445715d3c9973c7f2e113f65086711aae0
amirn003 <amir.naar@gmail.com>
2023-11-18 23:47:51 +0100

    set voice, activate speak based on voice selected

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ function populateVoices() {
 
   const voiceOptions = voices
     .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
 
   voicesDropdown.innerHTML = voiceOptions;
+}
 
+function setVoice() {
+  console.log(this.value);
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function toggle() {
+  speechSynthesis.cancel();
+  speechSynthesis.speak(msg);
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+voicesDropdown.addEventListener('change', setVoice);
